6306f5cf8ebf93d47409a2b3083aa9e49a71867e
MASAMIKI <masamiki.matsubara@gmail.com>
2017-02-19 16:11:03 +0900

    Initial commit

---
+++ app/src/main/java/com/example/masamiki_matsubara/petallogin/FBLogInActivity.java
@@ -0,0 +1,13 @@
+package com.example.masamiki_matsubara.petallogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FBLogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fblog_in);
+    }
+}
